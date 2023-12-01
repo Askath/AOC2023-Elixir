8125568c27c78efb2eacc18e6760fda363f1ab0a
Askath <taradruffel@outlook.com>
2023-12-01 20:55:12 +0100

    feat(first chapter): close to the solution

--- aoc1/lib/aoc1.ex
+++ aoc1/lib/aoc1.ex
@@ -16,26 +16,56 @@ defmodule Aoc1 do
     {String.first(string), String.last(string)}
   end
 
-  def hello do
-    {_, file} = File.read("input")
+  def read_file do
+    case File.read("input") do
+      {:ok, result} ->
+        result
 
-    Enum.map(String.split(file, "\n"), fn string ->
-      integers = Regex.scan(~r/\d+/, string)
+      {:error, error} ->
+        IO.puts("Error: #{error}")
+    end
+  end
 
-      Enum.map(integers, fn s ->
-        {first, last} =
-          Enum.map(s, fn t ->
-            first_and_last(t)
-          end)
-          |> Enum.at(0)
+  def replace_spoken_numbers(list) do
+    digits = [
+      {"one", 1},
+      {"two", 2},
+      {"three", 3},
+      {"four", 4},
+      {"five", 5},
+      {"six", 6},
+      {"seven", 7},
+      {"eight", 8},
+      {"nine", 9},
+      {"zero", 0}
+    ]
 
-        String.to_integer(first) + String.to_integer(last)
+    Enum.map(list, fn line ->
+      Enum.reduce(digits, line, fn {word, digit}, acc ->
+        String.replace(acc, word, Integer.to_string(digit))
       end)
-      |> Enum.at(0)
     end)
-    |> Enum.filter(fn nn -> nn != nil end)
-    |> Enum.sum()
-    |> IO.puts()
+  end
+
+  def parse(file) do
+    file |> String.split("\n", trim: true) |> replace_spoken_numbers()
+  end
+
+  def hello do
+    numbers =
+      parse(read_file())
+      |> Enum.map(fn line ->
+        Regex.replace(~r/[a-z]/, line, "") |> first_and_last()
+      end)
+
+    Enum.reduce(numbers, 0, fn {first, last}, acc ->
+      IO.puts("Previous: #{acc}")
+      IO.puts("First: #{first}")
+      IO.puts("Last: #{last}")
+      acc = acc + (String.to_integer(first) + String.to_integer(last))
+      IO.puts("After: #{acc}")
+      acc
+    end)
   end
 end
 
