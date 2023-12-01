86f83751310b8bfd00eb3c15fde05afbc290120e
Askath <taradruffel@outlook.com>
2023-12-01 22:09:22 +0100

    prepare for aoc2

---
+++ aoc2/lib/aoc2.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2 do
+  @moduledoc """
+  Documentation for `Aoc2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aoc2/test/aoc2_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2Test do
+  use ExUnit.Case
+  doctest Aoc2
+
+  test "greets the world" do
+    assert Aoc2.hello() == :world
+  end
+end
