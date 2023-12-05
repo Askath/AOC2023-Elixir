92d6d07be3a857e811aacfb78d747cfe874eacc8
Tara Druffel <t.druffel@outlook.com>
2023-12-05 15:28:19 +0100

    aoc2

--- aoc2/lib/aoc2.ex
+++ aoc2/lib/aoc2.ex
@@ -1,18 +1,30 @@
+# only 12 red cubes, 13 green cubes, and 14 blue cubes
 defmodule Aoc2 do
-  @moduledoc """
-  Documentation for `Aoc2`.
-  """
+  def parse_file do
+    file = File.read!("input") |> String.trim() |> String.split("\n")
+    file
+  end
 
-  @doc """
-  Hello world.
+  def game_number(string) do
+    game = String.split(string, "\n") |> Enum.at(0)
+    Regex.scan(~r/[0-9]:+/, game) |> Enum.at(0)
+  end
 
-  ## Examples
+  def sets(string) do
+    game = String.split(string, "\n") |> Enum.at(0)
+    set_string = String.split(game, ":") |> Enum.at(1)
+    String.split(set_string, ";", trim: true)
+  end
 
-      iex> Aoc2.hello()
-      :world
-
-  """
   def hello do
-    :world
+    parse_file()
   end
 end
+
+file = Aoc2.parse_file()
+
+Enum.each(file, fn x ->
+  game = Aoc2.game_number(x)
+  game_id = String.replace(Enum.at(game, 0), ":", "")
+  sets = Aoc2.sets(x)
+end)
