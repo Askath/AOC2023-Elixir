b0ee511dcf667c1b281f31ebebce6ba96617312b
Tara Druffel <t.druffel@outlook.com>
2023-12-05 16:23:04 +0100

    aoc3

---
+++ aoc3/lib/aoc3.ex
@@ -0,0 +1,18 @@
+defmodule Aoc3 do
+  @moduledoc """
+  Documentation for `Aoc3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aoc3/test/aoc3_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc3Test do
+  use ExUnit.Case
+  doctest Aoc3
+
+  test "greets the world" do
+    assert Aoc3.hello() == :world
+  end
+end
